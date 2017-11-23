e85c243b1d0a794649db22d20ebcf849efca7e01
amandeepkaursekhon <31941783+amandeepkaursekhon@users.noreply.github.com>
2017-11-22 20:24:13 -0500

    SIMPLE TABLE VIEW

--- MAPD-714-F2107/ViewController.swift
+++ MAPD-714-F2107/ViewController.swift
@@ -3,7 +3,7 @@ import UIKit
 
 class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
     
-    private let movies = ["Batman vs Superman", "Blade Runner 2049", "Thor: Ragnorok", "Justice League", "Wonderwoman", "Dark Tower", "Star Wars: The Last Jedi"]
+    private let movies = ["Batman vs Superman",  "Thor: Ragnorok", "Justice League", "Wonderwoman", "Dark Tower", "Star Wars: The Last Jedi","Blade Runner 2049"]
     let simpleTableIdentifier = "simpleTableIdentifier"
 
     override func viewDidLoad() {
@@ -17,15 +17,43 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         var cell = tableView.dequeueReusableCell(withIdentifier: simpleTableIdentifier)
         if (cell == nil){
-            cell = UITableViewCell(style:UITableViewCellStyle.default, reuseIdentifier: simpleTableIdentifier)
+            cell = UITableViewCell(style:UITableViewCellStyle.value1, reuseIdentifier: simpleTableIdentifier)
+            
         }
         
+        let image = UIImage(named: "star")
+        cell?.imageView?.image = image
+        let highlightedImage = UIImage(named :"star2")
+        cell?.imageView?.highlightedImage = highlightedImage
+        if (indexPath.row < 4) {
+            cell?.detailTextLabel?.text = "Superhero Movies"
+        }
+        else
+        {
+           cell?.detailTextLabel?.text = "Sci-Fi Movies"
+            
+        }
         cell?.textLabel?.text = movies[indexPath.row]
       
         
         return cell!
     }
     
-
+    func tableView(_ tableView: UITableView, indentationLevelForRowAt indexPath: IndexPath) -> Int {
+    return indexPath.row % 4
+    }
+    func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+        return indexPath.row == 0 ? nil :indexPath
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let rowValue = movies[indexPath.row]
+        let message = "you selected \(rowValue) "
+        let controller = UIAlertController(title:  "Row Selected", message: message, preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Yes I did!" ,style :.default ,handler: nil)
+        controller.addAction(action);
+present(controller, animated: true, completion: nil)
+    }
 }
 
