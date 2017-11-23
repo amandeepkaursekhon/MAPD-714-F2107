c3fc99c660f9ef5b53354129f67e3110ef69d98c
amandeepkaursekhon <31941783+amandeepkaursekhon@users.noreply.github.com>
2017-11-22 19:47:50 -0500

    created simple table view

--- MAPD-714-F2107/ViewController.swift
+++ MAPD-714-F2107/ViewController.swift
@@ -1,25 +1,31 @@
-//
-//  ViewController.swift
-//  MAPD-714-F2107
-//
-//  Created by Amandeep Sekhon on 2017-11-22.
-//  Copyright © 2017 Amandeep Sekhon. All rights reserved.
-//
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+    
+    private let movies = ["Batman vs Superman", "Blade Runner 2049", "Thor: Ragnorok", "Justice League", "Wonderwoman", "Dark Tower", "Star Wars: The Last Jedi"]
+    let simpleTableIdentifier = "simpleTableIdentifier"
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+      
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return movies.count
     }
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCell(withIdentifier: simpleTableIdentifier)
+        if (cell == nil){
+            cell = UITableViewCell(style:UITableViewCellStyle.default, reuseIdentifier: simpleTableIdentifier)
+        }
+        
+        cell?.textLabel?.text = movies[indexPath.row]
+      
+        
+        return cell!
+    }
+    
 
 }
 
